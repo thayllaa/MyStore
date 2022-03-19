5e8728672e10367ef44b7884742cdeaf04df2e8e
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-19 18:18:52 -0300

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -6,12 +6,9 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
+export class ProductAlertsComponent {
 
-  @Input() product!: Product;
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 
 }

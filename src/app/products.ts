cebc0e5fc47b5770b0f4cae8536d91b97a5f72be
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-19 18:08:31 -0300

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,13 +10,13 @@ export const products = [
     id: 1,
     name: 'ThayllaPhone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens.'
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras.'
   },
   {
     id: 3,
